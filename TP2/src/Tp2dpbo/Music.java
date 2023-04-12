f1e4b49e467b86d4429d498aeda17ae03e0484a4
Elsa Nabiilah <101001227+elsaa08@users.noreply.github.com>
2023-04-12 20:03:39 +0700

    Add files via upload

---
+++ TP2/src/Tp2dpbo/Music.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tp2dpbo;
+/*
+Saya [Elsa Nabiilah] [2108805] mengerjakan Tugas Praktikum 2 
+dalam mata kuliah Desain Pemrograman Berorientasi Objek untuk keberkahanNya 
+maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+*/
+/**
+ *
+ * @author elsan
+ */
+public class Music {
+    private int id_music;
+    private String title;
+    private String album;
+    private String producer;
+    private String img;
+    
+    public Music(){}
+    public Music(int id_music, String title, String album, String producer, String img){
+        this.id_music = id_music;
+        this.title = title;
+        this.album = album;
+        this.producer = producer;
+        this.img = img;
+        
+    }
+    public void setid(int id){
+        this.id_music = id_music;
+    }
+    public void settitle(String title){
+        this.title = title;
+    }
+    public void setalbum(String album){
+        this.album = album;
+    }
+    public void setproducer(String producer){
+        this.producer = producer;
+    }
+     public void setimg(String img){
+        this.img = img;
+    }
+    
+    public int getid(){
+        return id_music;
+    }
+    public String gettitle(){
+        return title;
+        
+    }
+    public String getalbum(){
+        return album;
+    }
+    public String getprod(){
+        return producer;
+    }
+    public String getimg(){
+        return this.img;
+    }
+    
+  
+}
---
+++ TP2/src/Tp2dpbo/dbConnection.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tp2dpbo;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+//import java.util.logging.Logger;
+/*
+Saya [Elsa Nabiilah] [2108805] mengerjakan Tugas Praktikum 2 
+dalam mata kuliah Desain Pemrograman Berorientasi Objek untuk keberkahanNya 
+maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+*/
+/**
+ *
+ * @author elsan
+ */
+public class dbConnection {
+   public static Statement stmt;
+   public static Connection conn;
+    
+private void connect(){
+    try{
+        String ConAddress = "jdbc:mysql://localhost/tp2";
+        String user = "root";
+        String pass = "";
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = (Connection) DriverManager.getConnection(ConAddress, user, pass);
+        stmt = (Statement) conn.createStatement();
+                
+    }catch(ClassNotFoundException | SQLException ex){
+    //    Logger.getLogger(dbConecction.class.getName()).Log()
+        System.out.println(ex.getMessage());
+        
+    }
+
+}
+
+public ResultSet selectQuery(String sql){
+    try{
+        stmt.executeQuery(sql);
+        return stmt.getResultSet();
+    }catch (SQLException ex){
+        System.out.println(ex.getMessage());
+    }
+    return null;
+
+}
+public int updateQuery(String sql){
+    try{
+        return stmt.executeUpdate(sql);
+    } catch (SQLException ex){
+        System.out.println(ex.getMessage());
+        
+    }
+    return 0;
+}
+
+public Statement getStatement(){
+    return stmt;
+}
+
+ public ResultSet Query(String in) {
+       // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       try{
+            connect();
+            String sql = in;
+            return stmt.executeQuery(sql);
+            
+        }catch(SQLException e){
+            System.err.println("Read gagal " +e.getMessage());
+        }
+        return null;
+    }
+
+    PreparedStatement prepareStatement(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
